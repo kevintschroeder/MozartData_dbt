8fe355146c32a4b4a25c700b1c347d6112808ddb
kevintschroeder <kevin.schroeder@candelarenewables.com>
2025-10-17 22:59:39 -0700

    Add mart model api_summary.sql

---
+++ models/marts/api_summary.sql
@@ -0,0 +1,17 @@
+-- models/marts/api_summary.sql
+
+with base as (
+    select * from {{ ref('stg_api_data') }}
+),
+
+aggregated as (
+    select
+        record_name,
+        count(*) as total_records,
+        avg(numeric_value) as avg_value,
+        max(updated_at) as last_updated
+    from base
+    group by 1
+)
+
+select * from aggregated
