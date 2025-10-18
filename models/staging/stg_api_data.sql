a3917ac699d34f6aa7f1949afe55fd5e8437a2eb
kevintschroeder <kevin.schroeder@candelarenewables.com>
2025-10-17 22:55:50 -0700

    Add staging model stg_api_data.sql

---
+++ models/staging/stg_api_data.sql
@@ -0,0 +1,16 @@
+-- models/staging/stg_api_data.sql
+
+with source as (
+    select * from {{ source('api_data', 'raw_data') }}
+),
+
+cleaned as (
+    select
+        id::string as record_id,
+        name::string as record_name,
+        value::float as numeric_value,
+        try_to_timestamp(updated_at) as updated_at
+    from source
+)
+
+select * from cleaned
