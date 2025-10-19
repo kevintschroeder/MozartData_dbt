85c1512ef665c41563efb9c37588a3f2e07d176d
kevintschroeder <kevin.schroeder@candelarenewables.com>
2025-10-19 11:32:33 -0700

    Update stg_api_data.sql

--- models/staging/stg_api_data.sql
+++ models/staging/stg_api_data.sql
@@ -1,16 +1,5 @@
 -- models/staging/stg_api_data.sql
 
-with source as (
-    select * from {{ source('api_data', 'raw_data') }}
-),
-
-cleaned as (
-    select
-        externalid::string as id,
-        name::string as record_name,
-        value::float as numeric_value,
-        try_to_timestamp(updated_at) as updated_at
-    from source
-)
-
-select * from cleaned
+select
+    externalid as id
+from api_ingest.raw_data
