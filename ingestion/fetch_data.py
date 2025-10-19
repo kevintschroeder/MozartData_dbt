400c5d6a7fae53cb0c59896502a05527e287de94
kevintschroeder <kevin.schroeder@candelarenewables.com>
2025-10-18 23:06:35 -0700

    Add dbt model and fetch_data API script

--- ingestion/fetch_data.py
+++ ingestion/fetch_data.py
@@ -1,75 +1,36 @@
-# Submit a request to Beanworks GraphQL
+# fetch_data.py
+import requests, json
 
-# GraphQL endpoint URL
-url_api = 'https://beanworks.ca/api/graphql/'
+def fetch(access_token, root_org_unit_id):
+    url_api = 'https://beanworks.ca/api/graphql/'
+    url_with_rouid = f'{url_api}?rouid={root_org_unit_id}'
 
-# Replace <RootOU> with the actual Root Organization Unit ID
-url_with_rouid = f'{url_api}?rouid={root_org_unit_id}'
-
-print(url_with_rouid)
-
-# Your GraphQL query (as a string)
-query = """
-{
-    invoices (where: {
-            _and: [{
-                    status: {
-                        _in: [Exported]
-                    }
-                },
-                {
-                    created:{
-                        _gt: "2024-06-09T00:00:00+00:00"
-                    }
-                }
-            ]
-        }
-  ) {
-    totalCount
-    edges {
-      node {
-            id
-            externalId
-            poNumber
-	    status          
-            created
-            number
-            owner{
-                id
-                fullName
-            }          
-            vendor {
-                structure{
-                    id
-                    name
+    query = """
+    {
+        invoices {
+            totalCount
+            edges {
+                node {
+                    externalId
                 }
             }
-            total
         }
     }
- }
-}
-"""
+    """
 
-# Set the headers for the request
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Http-Authorization': f'Bearer {access_token}'  # Bearer token for authorization
-}
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Http-Authorization': f'Bearer {access_token}'
+    }
 
-# Prepare the data to send in the body of the POST request
-data = {
-    'query': query
-}
+    data = {'query': query}
+    response = requests.post(url_with_rouid, headers=headers, data=data)
 
-# Send the POST request to the GraphQL API
-response = requests.post(url_with_rouid, headers=headers, data=data)
+    if response.status_code != 200:
+        raise Exception(f"GraphQL query failed: {response.status_code}\n{response.text}")
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the response as JSON and print the result
     result = response.json()
-    print(json.dumps(result, indent=2))  # Pretty print the result
-else:
-    print(f"GraphQL query failed with status code {response.status_code}")
-    print(response.text)
+    invoices = result["data"]["invoices"]["edges"]
+
+    # Return a list of dicts like [{'externalid': '12345'}, ...]
+    return [{"externalid": item["node"]["externalId"]} for item in invoices]
